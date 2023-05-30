bcbbc8264317570dfb5d8cde2fc79c399480abc4
Caio Machado <caioinnocencio@gmail.com>
2023-05-30 19:38:53 -0300

    Atualiza.

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const formulario = document.forms.entrada;
 formulario.addEventListener('submit', envia);
 
+let estado = 0;
+
 function envia(evento) {
     evento.preventDefault();
     console.log('forms enviado');
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }
